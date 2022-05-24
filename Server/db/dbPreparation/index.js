ef9adb764648e2f23c55ee678e461d9f1bca1c91
VIDO-home <vido1@yandex.ru>
2022-05-24 22:58:54 +0300

    Временное добавление оценок для всех заданий от администратора

--- Server/db/dbPreparation/index.js
+++ Server/db/dbPreparation/index.js
@@ -5,8 +5,14 @@ module.exports = async function dbPreparation(params) {
   await UserRole.findOrCreate({ where: { name: 'user' } })
   await UserRole.findOrCreate({ where: { name: 'administrator' } })
 
-  // const resu = await Task.scope('includeRating').findOne({
-  //   where: { id: 9 },
-  // })
-  // console.log(resu)
+  //!!! Убрать
+  //проставляем от имени администратора всем заданиям пятерки
+  const user = await User.findOne({
+    where: { email: process.env.DB_ADMINISTRATOR_EMAIL },
+  })
+  const tasks = await Task.findAll()
+
+  tasks.forEach(async (task) => {
+    await user.addRatingForTask(task, { through: { rating: 100 } })
+  })
 }
