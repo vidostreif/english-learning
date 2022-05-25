7aceb8777c08e4f9af9c5270755d0c4131c6d6fc
VIDO-home <vido1@yandex.ru>
2022-05-25 20:00:10 +0300

    Метод пересохранения старых картинок

--- Server/db/dbPreparation/index.js
+++ Server/db/dbPreparation/index.js
@@ -1,4 +1,8 @@
 const { UserRole, TaskRating, User, Task } = require('../models')
+const sharp = require('sharp')
+const uuid = require('uuid')
+const fs = require('fs')
+const path = require('path')
 
 //функция предварительного заполнения БД
 module.exports = async function dbPreparation(params) {
@@ -6,13 +10,59 @@ module.exports = async function dbPreparation(params) {
   await UserRole.findOrCreate({ where: { name: 'administrator' } })
 
   //!!! Убрать
-  //проставляем от имени администратора всем заданиям пятерки
-  const user = await User.findOne({
-    where: { email: process.env.DB_ADMINISTRATOR_EMAIL },
-  })
+  //Пересохраняем все картинки в новом формате
   const tasks = await Task.findAll()
 
   tasks.forEach(async (task) => {
-    await user.addRatingForTask(task, { through: { rating: 100 } })
+    if (task.imgUrl.indexOf('webp') < 0) {
+      task.imgUrl = await saveImg(task.imgUrl)
+      await task.save()
+    }
   })
+
+  //Удаляем все не нужные картинки
+  const tasks2 = await Task.findAll()
+
+  fs.readdir(path.resolve(__dirname, '../..', 'static'), (err, files) => {
+    files.forEach((file) => {
+      let del = true
+      tasks2.forEach(async (task) => {
+        if (task.imgUrl === file || 'mini_' + task.imgUrl === file) {
+          del = false
+        }
+      })
+
+      if (del) {
+        fs.unlink(path.resolve(__dirname, '../..', 'static', file), (err) => {
+          if (err) throw err
+        })
+
+        console.log(file)
+      }
+    })
+  })
+}
+
+const saveImg = async (oldfileName) => {
+  const newUuid = uuid.v4()
+  const fileName = newUuid + '.webp'
+  const oldImgPath = path.resolve(__dirname, '../..', 'static', oldfileName)
+  const imgPath = path.resolve(__dirname, '../..', 'static', fileName)
+  const imgPathMini = path.resolve(
+    __dirname,
+    '../..',
+    'static',
+    'mini_' + fileName
+  )
+  // img.mv(imgPath)
+
+  await sharp(oldImgPath).toFile(imgPath)
+  // сохраняем миниатюру
+  await sharp(oldImgPath).resize(350).toFile(imgPathMini)
+
+  fs.unlink(path.resolve(__dirname, '../..', 'static', oldfileName), (err) => {
+    if (err) throw err
+  })
+
+  return fileName
 }
