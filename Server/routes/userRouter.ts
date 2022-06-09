d05d057299bae8db30750c14aee2083d87420466
VIDO-home <vido1@yandex.ru>
2022-06-09 21:50:54 +0300

    Смена настроек

--- Server/routes/userRouter.ts
+++ Server/routes/userRouter.ts
@@ -8,13 +8,28 @@ import ApiError from '../exceptions/ApiError'
 router.post(
   '/registration',
   body('email').isEmail(), //валидация почты
-  body('password').isString().isLength({ min: 8, max: 32 }).not().isLowercase().not().isUppercase().not().isNumeric().not().isAlpha(), //валидация пароля
+  body('password')
+    .isString()
+    .isLength({ min: 8, max: 32 })
+    .not()
+    .isLowercase()
+    .not()
+    .isUppercase()
+    .not()
+    .isNumeric()
+    .not()
+    .isAlpha(), //валидация пароля
   ApiError.tryShell(userController.registration)
 )
 router.post('/login', ApiError.tryShell(userController.login))
 router.post('/logout', ApiError.tryShell(userController.logout))
 router.get('/activate/:link', ApiError.tryShell(userController.activate))
 router.get('/refresh', ApiError.tryShell(userController.refresh))
-router.get('/users', authMiddleware.isAuthorized, authMiddleware.isAdministarator, ApiError.tryShell(userController.getUsers))
+router.get(
+  '/users',
+  authMiddleware.isAuthorized,
+  authMiddleware.isAdministarator,
+  ApiError.tryShell(userController.getUsers)
+)
 
 export default router
